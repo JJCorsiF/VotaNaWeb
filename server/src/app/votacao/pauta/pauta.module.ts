2dd8a544de926d9bf9547863d80ce8d8a5b6b15b
João Corsi <jjcorsif@hotmail.com>
2024-01-22 22:01:57 -0300

    feat: :sparkles: adiciona funcionalidade para "abrir sessão de votação"

--- server/src/app/votacao/pauta/pauta.module.ts
+++ server/src/app/votacao/pauta/pauta.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Pauta } from '../persistence/pauta.entity';
+import { SessaoVotacao } from '../persistence/sessao-votacao.entity';
 import { PautaController } from './pauta.controller';
 import { PautaService } from './pauta.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Pauta])],
+  imports: [TypeOrmModule.forFeature([Pauta, SessaoVotacao])],
   controllers: [PautaController],
   providers: [PautaService],
 })
